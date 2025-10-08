cf145a5f7c4ed2599d62e6edf7d0044ebbc72925
Raphael Auberlet <ralphi2811@gmail.com>
2025-10-08 17:50:02 +0400

    🔧 Fix: base path pour GitHub Pages /raphaeldev/

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Repository à la racine pour raphaeldev.fr
+  base: '/raphaeldev/', // GitHub Pages avec nom de repo
   build: {
     outDir: 'dist',
   }
