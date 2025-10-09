6031a32c18fc53942c7b812e47a223e84b71dc15
Raphael Auberlet <ralphi2811@gmail.com>
2025-10-09 06:48:56 +0400

    🌐 Config pour domaine personnalisé raphaeldev.fr (base: '/')

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/raphaeldev/', // GitHub Pages avec nom de repo
+  base: '/', // Domaine personnalisé raphaeldev.fr
   build: {
     outDir: 'dist',
   }
